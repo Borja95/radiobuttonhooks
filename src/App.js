36f4e78ee375a0594558f242ba90ef471ffec013
Arturo Borja <abj951122@gmail.com>
2021-06-28 21:07:46 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FormGroup, Label, Input} from "reactstrap";
 
 function App() {
+  const[framework, setFramework] = useState(1);
+
+  const cambioRadioFramework=e=>{
+    setFramework(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <FormGroup>
+      <h5>¿Qué Framework estás utilizando?</h5>
+
+      <FormGroup className="formGroupRadios">
+        <FormGroup>
+          <Input
+            id="radio1"
+            type="radio"
+            value="1"
+            checked={framework == 1 ? true : false}
+            onChange={cambioRadioFramework}
+          />
+          <Label for="radio1">
+            React
+          </Label>
+        </FormGroup>
+
+        <FormGroup>
+          <Input
+            id="radio2"
+            type="radio"
+            value="2"
+            checked={framework == 2 ? true : false}
+            onChange={cambioRadioFramework}
+          />
+          <Label for="radio2">
+            Angular
+          </Label>
+        </FormGroup>
+
+        <FormGroup>
+          <Input
+            id="radio3"
+            type="radio"
+            value="3"
+            checked={framework == 3 ? true : false}
+            onChange={cambioRadioFramework}
+          />
+          <Label for="radio3">
+            Vue JS
+          </Label>
+        </FormGroup>
+
+        <FormGroup>
+          <Input
+            id="radio4"
+            type="radio"
+            value="4"
+            checked={framework == 4 ? true : false}
+            onChange={cambioRadioFramework}
+          />
+          <Label for="radio4">
+            Ember JS
+          </Label>
+        </FormGroup>
+
+      </FormGroup>
+
+     </FormGroup>
+
+     <br />
+     <p>El radio button seleccionado es: <b>{framework}</b></p>
     </div>
   );
 }
